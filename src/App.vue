8adbf4670f2d8ebedf4e491bad7fa9d46d1ebac0
andrew-soldat <blishch.andrew@gmail.com>
2023-09-07 22:58:46 +0300

    add localStorage and pushSate

--- src/App.vue
+++ src/App.vue
@@ -95,10 +95,45 @@
         </button>
       </section>
       <template v-if="tickers.length > 0">
+        <div>
+          <button
+            v-if="page > 1"
+            @click="page = page - 1"
+            type="button"
+            class="my-4 mr-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Назад
+          </button>
+          <button
+            v-if="hasNextPage"
+            @click="page = page + 1"
+            type="button"
+            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Вперед
+          </button>
+        </div>
+        <div>
+          <p>
+            <label
+              for="filter"
+              class="inline-block mr-1 text-sm font-medium text-gray-700"
+              >Фильтр:</label
+            >
+            <input
+              v-model="filter"
+              type="text"
+              id="filter"
+              name="filter"
+              class="pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+              placeholder=""
+            />
+          </p>
+        </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             @click="selectTiсker(t)"
             :key="t.name"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
@@ -188,9 +223,22 @@ export default {
       tickers: [],
       selectedTicker: null,
       graph: [],
+      page: 1,
+      filter: "",
+      hasNextPage: true,
     };
   },
   created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const tickersData = localStorage.getItem("tickers-list");
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
@@ -200,14 +248,23 @@ export default {
     }
   },
   methods: {
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+      const filteredTickers = this.tickers.filter((ticker) =>
+        ticker.name.toLowerCase().includes(this.filter.toLowerCase())
+      );
+      this.hasNextPage = filteredTickers.length > end;
+      return filteredTickers.slice(start, end);
+    },
     subscribeToUpdate(tickerName) {
       setInterval(async () => {
         const f = await fetch(
           `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&key=6d22c77eeb77aa8094333ff4f97d5a2a3edd8026068adc6a9d915761a7f8f498`
         );
         const data = await f.json();
-        this.tickers.find((t) => t.name === tickerName).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        // this.tickers.find((t) => t.name === tickerName).price =
+        //   data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
         if (this.selectedTicker?.name === tickerName) {
           this.graph.push(data.USD);
         }
@@ -223,6 +280,7 @@ export default {
         localStorage.setItem("tickers-list", JSON.stringify(this.tickers));
         this.subscribeToUpdate(newTicker.name);
         this.ticker = "";
+        this.filter = "";
       }
     },
     deleteTicker(tickerDelete) {
@@ -240,6 +298,24 @@ export default {
       }
       return this.graph.map(
         (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+  },
+  watch: {
+    filter() {
+      this.page = 1;
+
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
       );
     },
   },
