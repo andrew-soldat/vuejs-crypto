c1ddd7b753de5c53c21ab34d911685d0f453f689
andrew-soldat <blishch.andrew@gmail.com>
2023-11-01 09:07:32 +0300

    add component GraphTicker

--- src/App.vue
+++ src/App.vue
@@ -103,49 +103,13 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="selectedTicker" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }}
-        </h3>
-        <div
-          class="flex items-end border-gray-600 border-b border-l h-64"
-          ref="graph"
-        >
-          <div
-            v-for="(bar, index) in normalizedGraph"
-            :key="index"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-          @click="selectedTicker = null"
-          type="button"
-          class="absolute top-0 right-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <graph-ticker
+        v-if="selectedTicker"
+        @click="closeTicker"
+        @calculatedMaxGraphElements="calculatedMaxGraphElements"
+        :ticker="selectedTicker"
+        :graph="graph"
+      />
     </div>
   </div>
 </template>
@@ -153,10 +117,11 @@
 <script>
 import { subscribeToTicker, unsubscribeFromTicker } from "@/api";
 import AddTicker from "@/components/AddTicker";
+import GraphTicker from "@/components/GraphTicker";
 
 export default {
   name: "App",
-  components: { AddTicker },
+  components: { GraphTicker, AddTicker },
   data() {
     return {
       tickers: [],
@@ -167,6 +132,7 @@ export default {
       maxGraphElements: 1,
     };
   },
+  emits: ["close-ticker", "calculated-max-graph-elements"],
   created() {
     const windowData = Object.fromEntries(
       new URL(window.location).searchParams.entries()
@@ -215,18 +181,6 @@ export default {
     hasNextPage() {
       return this.filteredTickers.length > this.endIndex;
     },
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-
-      if (maxValue === minValue) {
-        return this.graph.map(() => 50);
-      }
-
-      return this.graph.map(
-        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
-    },
     pageStateOption() {
       return {
         filter: this.filter,
@@ -235,11 +189,15 @@ export default {
     },
   },
   methods: {
-    calculatedMaxGraphElements() {
-      if (!this.$refs.graph) {
+    calculatedMaxGraphElements(width) {
+      if (!width) {
         return;
       }
-      this.maxGraphElements = this.$refs.graph.offsetWidth / 40;
+      this.maxGraphElements = width / 40;
+    },
+    closeTicker() {
+      this.$emit("close-ticker");
+      this.selectedTicker = null;
     },
     updateTicker(tickerName, price) {
       this.tickers
@@ -280,16 +238,9 @@ export default {
       unsubscribeFromTicker(tickerDelete.name);
     },
   },
-  mounted() {
-    window.addEventListener("resize", this.calculatedMaxGraphElements);
-  },
-  beforeUnmount() {
-    window.removeEventListener("resize", this.calculatedMaxGraphElements);
-  },
   watch: {
     selectedTicker() {
       this.graph = [];
-      this.$nextTick().then(this.calculatedMaxGraphElements);
     },
     paginatedTickers() {
       if (this.paginatedTickers.length === 0 && this.page > 1) {
